9beaf46137a1b8c97ed3eaec8b8e3126c36af296
Feynman <2901954062@qq.com>
2022-11-09 16:32:48 +0800

    feat: 12 add

---
+++ TypeScript_TODO/packages/12-covariance-and-contravariance/base.ts
@@ -0,0 +1,43 @@
+/*
+ * @Author: fg
+ * @Date: 2022-11-09 16:08:06
+ * @LastEditors: fg
+ * @LastEditTime: 2022-11-09 16:21:55
+ * @Description: base
+ */
+class Animal {
+  asPet() { }
+}
+
+class Dog extends Animal {
+  bark() { }
+}
+
+class Corgi extends Dog {
+  cute() { }
+}
+
+type DogFactory = (args: Dog) => Dog;
+
+function transformDogAndBark(dogFactory: DogFactory) {
+  const dog = dogFactory(new Dog());
+  dog.bark();
+}
+
+function makeDogBark(dog: Dog) {
+  dog.bark();
+}
+
+// 没问题
+makeDogBark(new Corgi());
+// 不行
+makeDogBark(new Animal());
+
+type AsFuncArgType<T> = (arg: T) => void;
+type AsFuncReturnType<T> = (arg: unknown) => T;
+
+// 1.成立：（T->Corgi）<=(T->Dog)
+type CheckReturnType = AsFuncReturnType<Corgi> extends AsFuncReturnType<Dog> ? 1 : 2;
+
+// 2.不成立：（Dog->T）<=(Animal -> T)
+type CheckArgType = AsFuncArgType<Dog> extends AsFuncArgType<Animal> ? 1 : 2;
---
+++ TypeScript_TODO/packages/12-covariance-and-contravariance/compare.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: fg
+ * @Date: 2022-11-09 16:25:57
+ * @LastEditors: fg
+ * @LastEditTime: 2022-11-09 16:30:57
+ * @Description: compare
+ */
+
+function fn(dog: Dog) {
+  dog.bark();
+}
+
+type CorgiFunc = (input: Corgi) => void;
+type AnimalFunc = (input: Animal) => void;
+
+const func1: CorgiFunc = fn
+
+const func2: AnimalFunc = fn
+
+export { };
