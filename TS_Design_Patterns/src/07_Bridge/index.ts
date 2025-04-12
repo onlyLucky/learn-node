3b92d1ebc007eba5dbc30962ebf9a8aee12c46bb
Feynman <2901954062@qq.com>
2025-04-12 18:02:27 +0800

    feat: upload code.

---
+++ TS_Design_Patterns/src/07_Bridge/index.ts
@@ -0,0 +1,49 @@
+/* 桥接模式 */
+
+abstract class Abstraction {
+  protected implementation: Implementation;
+
+  constructor(implementation: Implementation) {
+    this.implementation = implementation;
+  }
+
+  public abstract operation(): string;
+}
+
+class ExtendedAbstraction extends Abstraction {
+  public operation(): string {
+    const result = this.implementation.operationImplementation();
+    return `ExtendedAbstraction: Extended operation with:\n${result}`;
+  }
+}
+
+interface Implementation {
+  operationImplementation(): string;
+}
+
+class ConcreteImplementationA implements Implementation {
+  public operationImplementation(): string {
+    return 'ConcreteImplementationA: Here\'s the result on the platform A.';
+  }
+}
+
+class ConcreteImplementationB implements Implementation {
+  public operationImplementation(): string {
+    return 'ConcreteImplementationB: Here\'s the result on the platform B.';
+  }
+}
+
+function clientCode(abstraction: Abstraction) {
+  console.log(abstraction.operation());
+}
+
+// 测试桥接模式
+let implementation = new ConcreteImplementationA();
+let abstraction = new Abstraction(implementation);
+clientCode(abstraction);
+
+console.log('');
+
+implementation = new ConcreteImplementationB();
+abstraction = new ExtendedAbstraction(implementation);
+clientCode(abstraction);
