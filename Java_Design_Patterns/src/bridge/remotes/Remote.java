7bfebe22a963f8bdb84e3c2480de51af372f808f
Feynman <2901954062@qq.com>
2025-04-29 17:58:57 +0800

    feat: 适配器模式 桥接模式。

---
+++ Java_Design_Patterns/src/bridge/remotes/Remote.java
@@ -0,0 +1,17 @@
+package bridge.remotes;
+
+/* 
+遥控器
+*/
+
+public interface Remote {
+  void power();
+
+  void volumeDown();
+
+  void volumeUp();
+
+  void channelDown();
+
+  void channelUp();
+}
